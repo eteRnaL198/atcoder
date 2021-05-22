73102a21a65fe06417aecfefc08115c1f64cae60
eternal198 <ikuya313198@gmail.com>
2021-05-22 16:32:11 +0900

    update

--- Beginners Selection/CardGameforTwo.js
+++ Beginners Selection/CardGameforTwo.js
@@ -1,15 +1,19 @@
 "use strict";
 const main = input => {
   input = input.split('\n');
-  const nums = input[1].split('').map(num => parseInt(num));
+  const nums = input[1].split(' ').map(num => parseInt(num));
   const sortedNums = nums.map((elem, idx, nums) => {
-    if(idx ===0) {
-      for(let j=nums.length-1; j>0; j--) {
-        if(nums[j-1] < nums[j]) nums.splice(j, 2, nums[j], nums[j-1]);
-      }
+    for(let j=nums.length-1; j>idx; j--) {
+      if(nums[j-1] < nums[j]) nums.splice(j-1, 2, nums[j], nums[j-1]);
     }
-    return elem;
+    return nums[idx];
   });
-  console.log(sortedNums);
+  const winner = sortedNums.reduce((acc, cur, idx) => (
+    (idx % 2 === 0) ? acc + cur : acc
+  ), 0);
+  const loser = sortedNums.reduce((acc, cur, idx) => (
+    (idx % 2 !== 0) ? acc + cur : acc
+  ), 0)
+  console.log(winner - loser);
 }
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
